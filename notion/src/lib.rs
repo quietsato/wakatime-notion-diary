5c288f2a6e8d122a88ff4da628aa33099f0da796
Taiki Sato <quietsato@gmail.com>
2023-08-29 21:46:53 +0900

    feat(notion): export modules

--- notion/src/lib.rs
+++ notion/src/lib.rs
@@ -3,9 +3,10 @@ mod database;
 mod page;
 
 pub use api::NotionApi;
-pub use database::get_page::{
-    error::GetPageError,
-    response::{GetPageResponse, Page},
-    GetPage,
+pub use database::create_page::{error::CreatePageError, response::CreatePageResponse, CreatePage};
+pub use database::get_or_create_page::{
+    error::GetOrCreatePageError, response::GetOrCreatePageResponse, GetOrCreatePage,
 };
+pub use database::get_page::{error::GetPageError, response::GetPageResponse, GetPage};
+pub use database::models::page::Page;
 pub use page::append_blocks::{AppendBlocks, AppendBlocksError};
